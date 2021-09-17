8e05cb02568be2ce4f854027790f995e8640a1dc
anoopk-extentia <84086500+anoopk-extentia@users.noreply.github.com>
2021-09-17 20:12:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQSSendMessages
+{
+  // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+  // Class to send messages to a queue
+  class Program
+  {
+    // Some example messages to send to the queue
+    private const string JsonMessage = "{\"product\":[{\"name\":\"Product A\",\"price\": \"32\"},{\"name\": \"Product B\",\"price\": \"27\"}]}";
+
+    static async Task Main(string[] args)
+    {
+      // Do some checks on the command-line
+      if(args.Length == 0)
+      {
+        Console.WriteLine("\nUsage: SQSSendMessages queue_url");
+        Console.WriteLine("   queue_url - The URL of an existing SQS queue.");
+        return;
+      }
+      if(!args[0].StartsWith("https://sqs."))
+      {
+        Console.WriteLine("\nThe command-line argument isn't a queue URL:");
+        Console.WriteLine($"{args[0]}");
+        return;
+      }
+
+    const string AWS_KEY = "";
+    const string AWS_SECRET = "";
+      // Create the Amazon SQS client
+      var sqsClient = new AmazonSQSClient(AWS_KEY, AWS_SECRET, Amazon.RegionEndpoint.APSouth1);
+
+      // (could verify that the queue exists)
+      // Send some example messages to the given queue
+      // A single message
+      await SendMessage(sqsClient, args[0], JsonMessage);
+
+      // Delete all messages that are still in the queue
+      await DeleteAllMessages(sqsClient, args[0]);
+    }
+
+
+    //
+    // Method to put a message on a queue
+    // Could be expanded to include message attributes, etc., in a SendMessageRequest
+    private static async Task SendMessage(
+      IAmazonSQS sqsClient, string qUrl, string messageBody)
+    {
+      SendMessageResponse responseSendMsg =
+        await sqsClient.SendMessageAsync(qUrl, messageBody);
+      Console.WriteLine($"Message added to queue\n  {qUrl}");
+      Console.WriteLine($"HttpStatusCode: {responseSendMsg.HttpStatusCode}");
+    }
+
+    //
+    // Method to delete all messages from the queue
+    private static async Task DeleteAllMessages(IAmazonSQS sqsClient, string qUrl)
+    {
+      Console.WriteLine($"\nPurging messages from queue\n  {qUrl}...");
+      PurgeQueueResponse responsePurge = await sqsClient.PurgeQueueAsync(qUrl);
+      Console.WriteLine($"HttpStatusCode: {responsePurge.HttpStatusCode}");
+    }
+  }
+}
